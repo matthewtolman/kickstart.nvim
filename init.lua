0de41aff38bd998c60e5f44709ee742a4314cbd1
Matt Tolman <matttolman@Matts-MacBook-Pro.local>
2024-05-05 11:05:20 -0600

    disabled fold on open

--- init.lua
+++ init.lua
@@ -876,30 +876,5 @@ cmp.setup.filetype('markdown', { sources = {} })
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 
---- opens folds on file open
-local M = {}
--- function to create a list of commands and convert them to autocommands
--------- This function is taken from https://github.com/norcalli/nvim_utils
-function M.nvim_create_augroups(definitions)
-  for group_name, definition in pairs(definitions) do
-    vim.api.nvim_command('augroup ' .. group_name)
-    vim.api.nvim_command('autocmd!')
-    for _, def in ipairs(definition) do
-      local command = table.concat(vim.tbl_flatten { 'autocmd', def }, ' ')
-      vim.api.nvim_command(command)
-    end
-    vim.api.nvim_command('augroup END')
-  end
-end
-
-local autoCommands = {
-  -- other autocommands
-  open_folds = {
-    { "BufReadPost,FileReadPost", "*", "normal zR" }
-  }
-}
-
-M.nvim_create_augroups(autoCommands)
-
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
