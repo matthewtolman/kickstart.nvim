83d29f177dc96222d500beb3c97f3ed95680108b
mtolman <matthewstolman@gmail.com>
2025-10-15 09:25:50 -0600

    updated tabstop

--- init.lua
+++ init.lua
@@ -90,6 +90,10 @@ P.S. You can delete this when you're done too. It's your config now! :)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+vim.o.expandtab = true
+vim.o.shiftwidth = 2
+vim.o.tabstop = 2
+
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = false
 
